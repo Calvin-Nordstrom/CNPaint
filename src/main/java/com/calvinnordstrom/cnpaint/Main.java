da350fac43030bdfe7b1226339255cf6ec63ec4a
Calvin Nordstrom <calnordstrom@icloud.com>
2025-04-24 22:21:38 -0400

    refactor: removed line count printing

--- src/main/java/com/calvinnordstrom/cnpaint/Main.java
+++ src/main/java/com/calvinnordstrom/cnpaint/Main.java
@@ -8,14 +8,8 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.Objects;
 import java.util.logging.Logger;
-import java.util.stream.Stream;
 
 public class Main extends Application {
     public static final Logger LOGGER = Logger.getLogger(Main.class.getPackageName());
@@ -37,39 +31,11 @@ public class Main extends Application {
         stage.setScene(scene);
         stage.setMaximized(true);
         stage.show();
-
-        printLineCount();
     }
 
     @Override
     public void stop() {
         System.out.println("Prompt for save or exit");
-    }
-
-    private static void printLineCount() {
-        Path rootDir = Paths.get("C:\\Users\\calno\\Desktop\\IntelliJ Projects\\CNPaint\\src\\main\\java\\com\\calvinnordstrom\\cnpaint");
-        try (Stream<Path> paths = Files.walk(rootDir)) {
-            long totalLines = paths
-                    .filter(Files::isRegularFile)
-                    .filter(path -> path.toString().endsWith(".java"))
-                    .mapToLong(Main::countLinesInFile)
-                    .sum();
-            System.out.println("Total lines in all .java files: " + totalLines);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private static long countLinesInFile(Path path) {
-        long lineCount = 0;
-        try (BufferedReader reader = Files.newBufferedReader(path)) {
-            while (reader.readLine() != null) {
-                lineCount++;
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return lineCount;
     }
 
     public static void main(String[] args) {
